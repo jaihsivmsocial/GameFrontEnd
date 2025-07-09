5329bb7b25506b7ca13b247e3bacf3abb28bb51d
jaihsivmsocial <jaishiv@msocaialverse.com>
2025-07-09 18:58:50 +0530

    done

--- src/utils/apiservice.jsx
+++ src/utils/apiservice.jsx
@@ -1,6 +1,6 @@
 
-// export const BASEURL = "https://apitest.tribez.gg"
-export const BASEURL = "http://localhost:5000"
+export const BASEURL = "https://apitest.tribez.gg"
+// export const BASEURL = "http://localhost:5000"
 // export const BASEURL="http://13.60.238.38"
 
  
