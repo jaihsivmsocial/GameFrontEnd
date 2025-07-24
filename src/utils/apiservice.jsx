c91eedccd3ffab11aceb5bd017d023877fd7d90f
jaihsivmsocial <jaishiv@msocaialverse.com>
2025-07-24 10:33:54 +0530

    done

--- src/utils/apiservice.jsx
+++ src/utils/apiservice.jsx
@@ -1,6 +1,6 @@
 
-// export const BASEURL = "http://api.5mof.gg"
-export const BASEURL = "http://localhost:5000"
+export const BASEURL = "http://api.5mof.gg"
+// export const BASEURL = "http://localhost:5000"
 // export const BASEURL="http://13.60.238.38"
 
  
